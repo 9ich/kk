d49025859af7e1e34ee636c580e99af538e2f741
ich <sunn>
2016-02-27 23:39:23 +0000

    shared: set gamename defines

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -27,17 +27,17 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // A user mod should never modify this file
 
 #ifdef STANDALONE
-  #define PRODUCT_NAME				"iofoo3"
-  #define BASEGAME					"foobar"
-  #define CLIENT_WINDOW_TITLE		"changeme"
-  #define CLIENT_WINDOW_MIN_TITLE	"changeme2"
-  #define HOMEPATH_NAME_UNIX		".foo"
-  #define HOMEPATH_NAME_WIN			"FooBar"
+  #define PRODUCT_NAME				"konkrete"
+  #define BASEGAME					"konkrete"
+  #define CLIENT_WINDOW_TITLE		"Konkrete"
+  #define CLIENT_WINDOW_MIN_TITLE	"Konkrete"
+  #define HOMEPATH_NAME_UNIX		".konkrete"
+  #define HOMEPATH_NAME_WIN			"konkrete"
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
 //  #define STEAMPATH_NAME			"Foo Bar"
 //  #define STEAMPATH_APPID         ""
-  #define GAMENAME_FOR_MASTER		"foobar"	// must NOT contain whitespace
-  #define CINEMATICS_LOGO		"foologo.roq"
+  #define GAMENAME_FOR_MASTER		"konkrete"	// must NOT contain whitespace
+  #define CINEMATICS_LOGO		"logo.roq"
   #define CINEMATICS_INTRO		"intro.roq"
 //  #define LEGACY_PROTOCOL	// You probably don't need this for your standalone game
 #else
