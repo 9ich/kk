b2cfad5281cea14bb7443fa5496e35382ec56ceb
ich <sunn>
2016-04-08 16:11:22 +0100

    renderer: dim defaultimage

--- code/renderergl2/tr_image.c
+++ code/renderergl2/tr_image.c
@@ -2655,33 +2655,33 @@ static void R_CreateFogImage( void ) {
 R_CreateDefaultImage
 ==================
 */
-#define	DEFAULT_SIZE	16
+#define	DEFAULT_SIZE	32
 static void R_CreateDefaultImage( void ) {
 	int		x;
 	byte	data[DEFAULT_SIZE][DEFAULT_SIZE][4];
 
 	// the default image will be a box, to allow you to see the mapping coordinates
-	Com_Memset( data, 32, sizeof( data ) );
+	Com_Memset( data, 0x40, sizeof( data ) );
 	for ( x = 0 ; x < DEFAULT_SIZE ; x++ ) {
 		data[0][x][0] =
 		data[0][x][1] =
 		data[0][x][2] =
-		data[0][x][3] = 255;
+		data[0][x][3] = 0x7F;
 
 		data[x][0][0] =
 		data[x][0][1] =
 		data[x][0][2] =
-		data[x][0][3] = 255;
+		data[x][0][3] = 0x7F;
 
 		data[DEFAULT_SIZE-1][x][0] =
 		data[DEFAULT_SIZE-1][x][1] =
 		data[DEFAULT_SIZE-1][x][2] =
-		data[DEFAULT_SIZE-1][x][3] = 255;
+		data[DEFAULT_SIZE-1][x][3] = 0x7F;
 
 		data[x][DEFAULT_SIZE-1][0] =
 		data[x][DEFAULT_SIZE-1][1] =
 		data[x][DEFAULT_SIZE-1][2] =
-		data[x][DEFAULT_SIZE-1][3] = 255;
+		data[x][DEFAULT_SIZE-1][3] = 0x7F;
 	}
 	tr.defaultImage = R_CreateImage("*default", (byte *)data, DEFAULT_SIZE, DEFAULT_SIZE, IMGTYPE_COLORALPHA, IMGFLAG_MIPMAP, 0);
 }
