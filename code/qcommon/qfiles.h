dbae46c9c93350ea10bdea591dbedbb3ab6c8d4b
ich <sunn>
2016-07-09 23:14:32 +0100

    common: increase MD3_MAX_TAGS

--- code/qcommon/qfiles.h
+++ code/qcommon/qfiles.h
@@ -87,7 +87,7 @@ typedef struct {
 #define MD3_MAX_SHADERS		256		// per surface
 #define MD3_MAX_FRAMES		1024	// per model
 #define	MD3_MAX_SURFACES	32		// per model
-#define MD3_MAX_TAGS		16		// per frame
+#define MD3_MAX_TAGS		64		// per frame	// (konkrete: was 16)
 
 // vertex scales
 #define	MD3_XYZ_SCALE		(1.0/64)
