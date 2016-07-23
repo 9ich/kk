87c48b0929fa6e7cbd05d45005cba7e617cecc2d
ich <sunn>
2016-07-23 19:08:22 +0100

    snd: new cvar defaults for openal

--- code/client/snd_openal.c
+++ code/client/snd_openal.c
@@ -2512,10 +2512,10 @@ qboolean S_AL_Init( soundInterface_t *si )
 	s_alGain = Cvar_Get( "s_alGain", "1.0", CVAR_ARCHIVE );
 	s_alSources = Cvar_Get( "s_alSources", "96", CVAR_ARCHIVE );
 	s_alDopplerFactor = Cvar_Get( "s_alDopplerFactor", "1.0", CVAR_ARCHIVE );
-	s_alDopplerSpeed = Cvar_Get( "s_alDopplerSpeed", "9000", CVAR_ARCHIVE );
-	s_alMinDistance = Cvar_Get( "s_alMinDistance", "120", CVAR_CHEAT );
-	s_alMaxDistance = Cvar_Get("s_alMaxDistance", "1024", CVAR_CHEAT);
-	s_alRolloff = Cvar_Get( "s_alRolloff", "2", CVAR_CHEAT);
+	s_alDopplerSpeed = Cvar_Get( "s_alDopplerSpeed", "15000", CVAR_ARCHIVE );
+	s_alMinDistance = Cvar_Get( "s_alMinDistance", "800", CVAR_CHEAT );
+	s_alMaxDistance = Cvar_Get("s_alMaxDistance", "10000", CVAR_CHEAT);
+	s_alRolloff = Cvar_Get( "s_alRolloff", "0.5", CVAR_CHEAT);
 	s_alGraceDistance = Cvar_Get("s_alGraceDistance", "512", CVAR_CHEAT);
 
 	s_alDriver = Cvar_Get( "s_alDriver", ALDRIVER_DEFAULT, CVAR_ARCHIVE | CVAR_LATCH );
