cc67e6dc7e21511e14f6e8b4d763181e5f1aa71d
ich <sunn>
2017-04-14 17:32:22 +0100

    bg: keep decrementing weapontime past 0 while fire is held

--- code/game/bg_pmove.c
+++ code/game/bg_pmove.c
@@ -744,8 +744,7 @@ pmweapevents(int slot)
 		pm->ps->pm_flags &= ~PMF_USE_ITEM_HELD;
 
 	// make weapon function
-	if(pm->ps->weaponTime[slot] > 0)
-		pm->ps->weaponTime[slot] -= pml.msec;
+	pm->ps->weaponTime[slot] -= pml.msec;
 
 	// check for weapon change
 	// can't change if weapon is firing, but can change
